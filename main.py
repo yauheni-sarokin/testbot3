b1a2f6666e30500d7cb15c89c57c183e5b694772
snoop187dog <snoop187dog@mail.ru>
2019-08-19 17:35:31 +0200

    commit #4

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ bot =  telebot.TeleBot("759571628:AAFtmCaDudbElJMu9COkXhTMjUFSc5wXDtY")
 def send_welcome(message):
 	#bot.reply_to(message, "Howdy, how are you doing?")
 	bot.send_message(message.chat.id,"responce: " +  message.text)
-	if message.text == "lol":
+	if message.text == "id":
 #		id = str(2)
 #		type(id)
 #		print("wow you know it: " + str(message.chat.id))
 #		print("wow you know it: " + id)
 		bot.send_message(message.chat.id, "wow you know it, your id is : " + str(message.chat.id))
-
+	elif message.text == "хуй":
+		bot.send_message(message.chat.id, "cам хуй!")
 #	print(message.text)
+
+
 
 bot.polling( none_stop = True )
